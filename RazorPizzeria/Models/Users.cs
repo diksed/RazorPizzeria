8ab610f973322152fdd2a029af2139c074a8504c
diksed <diksed00@gmail.com>
2022-08-05 14:22:39 +0300

    Revert "Made some fixes about Login Page"
    This reverts commit cff5c83b5f8011a5abe51fd4381abf01382d2e34.

--- RazorPizzeria/Models/Users.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RazorPizzeria.Models
-{
-    public class Users
-    {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
