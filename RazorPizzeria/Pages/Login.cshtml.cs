78e53158b0bda44ce4984fc11f3941bcb0ae3100
diksed <diksed00@gmail.com>
2022-08-05 14:24:33 +0300

    Revert "Revert "Made some fixes about Login Page""
    This reverts commit 8ab610f973322152fdd2a029af2139c074a8504c.

--- RazorPizzeria/Pages/Login.cshtml.cs
+++ RazorPizzeria/Pages/Login.cshtml.cs
@@ -1,12 +1,45 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using RazorPizzeria.Data;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
 
 namespace RazorPizzeria.Pages
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public Credential Credential { get; set; }
         public void OnGet()
         {
         }
+        public async Task <IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+            if(Credential.Username == "admin" && Credential.Password == "123")
+            {
+                var claims = new List<Claim> {
+                new Claim(ClaimTypes.Name, "admin"),
+                new Claim(ClaimTypes.Email, "admin@mywebsite.com")
+                };
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+                return RedirectToPage("/Index");
+            }
+            return Page();
+        }
+    }
+    public class Credential
+    {
+        [Required]
+        [Display(Name ="User Name")]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
