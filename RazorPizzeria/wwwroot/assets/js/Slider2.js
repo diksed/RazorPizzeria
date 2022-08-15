3ceea21bb7a69c82b95b5f55874f55a6a47839d9
diksed <diksed00@gmail.com>
2022-08-15 16:42:50 +0300

    Added slider feature to drinks section

---
+++ RazorPizzeria/wwwroot/assets/js/Slider2.js
@@ -0,0 +1,32 @@
+﻿var aautoplayInterval = 7500;
+var aautoplayTimer = null;
+var aautoplay = true;
+var nnewIndex = 1;
+
+if (aautoplay) {
+    aautoplayTimer = setInterval(function () {
+        nnewIndex++;
+        navigateSSlider();
+    }, aautoplayInterval);
+}
+
+function resetSSlider() {
+    clearInterval(aautoplayTimer);
+}
+
+function navigateSSlider() {
+    const sslide1 = document.getElementById('sslide1');
+    const sslide2 = document.getElementById('sslide2');
+    const sslide3 = document.getElementById('sslide3');
+    const sslide4 = document.getElementById('sslide4');
+    if (nnewIndex == 1) {
+        sslide1.checked = true;
+    } else if (nnewIndex == 2) {
+        sslide2.checked = true;
+    } else if (nnewIndex == 3) {
+        sslide3.checked = true;
+    } else if (nnewIndex == 4) {
+        sslide4.checked = true;
+        nnewIndex = 0;
+    }
+}
